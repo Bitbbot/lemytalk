33c7e4c7085cdea5f24392d6bb7b505a8b7a9bfb
BitBot <tbmn2017@gmail.com>
2022-10-12 19:52:28 +0300

    refactor: create new component for Local stream & make layout flexible

---
+++ client/src/components/Window1/LocalVideo/LocalVideo.js
@@ -0,0 +1,37 @@
+import { observer } from "mobx-react-lite";
+import { useContext, useEffect, useRef } from "react";
+import { Context } from "../../../index";
+import s from "./LocalVideo.module.scss";
+import noise from "../../../assets/img/Untitled8.svg";
+
+const LocalVideo = observer(() => {
+    const { user } = useContext(Context);
+    const localVideoRef = useRef();
+    useEffect(() => {
+        if (user.localStream) {
+            const localVideo = localVideoRef.current;
+            localVideo.srcObject = user.localStream;
+            localVideo.onLoadedMetadata = () => {
+                localVideo.play();
+            };
+        }
+    }, [user.localStream]);
+    return (
+        <div className={s.wrapper}>
+            {user.localStream ? (
+                <div className={s.video_wrapper}>
+                    <video
+                        className={s.video}
+                        ref={localVideoRef}
+                        autoPlay
+                        muted
+                        width="100%"
+                    ></video>
+                </div>
+            ) : (
+                <img src={noise} width="100%" className={s.img} />
+            )}
+        </div>
+    );
+});
+export default LocalVideo;

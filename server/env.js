54df75008b38d69ded2630aa0f48b36e44db09cf
BitBot <tbmn2017@gmail.com>
2022-10-19 10:20:08 +0300

    commit for deployment

--- server/env.js
+++ server/env.js
@@ -2,7 +2,11 @@ export const CLIENT_URL = "http://localhost:3000";
 export const SERVER_URL = "http://localhost:5000";
 export const DB_NAME = "lemytalk";
 export const DB_USER = "postgres";
+// export const DB_PASSWORD = "";
+// export const DB_HOST = "2.57.185.108";
 export const DB_PASSWORD = "root";
 export const DB_HOST = "localhost";
 export const DB_PORT = 5432;
 export const PORT = 5000;
+// 127.0.0.1:5432
+// password = lemytalkdatabasepassword
